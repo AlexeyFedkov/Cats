791e37278f79042dcfcab964bd024d9e58d62e5f
Ruslan <sadykovlq@gmail.com>
2020-05-02 15:06:03 +0500

    bla bla

---
+++ Assets/EnemyView.cs
@@ -0,0 +1,19 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyView : MonoBehaviour
+{
+  
+    private void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up);
+
+        Debug.DrawRay(transform.position, transform.up * 10, Color.red);
+
+        if (hit)
+        {
+            Debug.Log("Spotted!");
+        }
+    }
+}
