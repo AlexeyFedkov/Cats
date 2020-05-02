465886db06d84b6ae5a278d915bf65961361122f
frolushka <mpfrolovstud@gmail.com>
2020-05-02 18:46:28 +0300

    add cat animation

--- Assets/Scripts/IsometricPlayerMovementController.cs
+++ Assets/Scripts/IsometricPlayerMovementController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+ [RequireComponent(typeof(SpriteRenderer))]
 public class IsometricPlayerMovementController : MonoBehaviour
 {
 
@@ -10,17 +11,27 @@ public class IsometricPlayerMovementController : MonoBehaviour
 
     Rigidbody2D rbody;
 
+    private SpriteRenderer[] _spriteRenderers;
+    private Animator[] _animators;
+    
     private void Awake()
     {
         rbody = GetComponent<Rigidbody2D>();
+        _animators = GetComponentsInChildren<Animator>();
+        _spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
         // isoRenderer = GetComponentInChildren<IsometricCharacterRenderer>();
     }
 
     void FixedUpdate()
     {
         Vector2 currentPos = rbody.position;
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        for (int i = 0; i < _animators.Length; i++)
+        {
+            _spriteRenderers[i].flipX = horizontalInput < 0;
+            _animators[i].SetBool("move", !(Mathf.Approximately(horizontalInput, 0) && Mathf.Approximately(verticalInput, 0)));
+        }
         Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
         inputVector = Vector2.ClampMagnitude(inputVector, 1);
         Vector2 movement = inputVector * movementSpeed;
