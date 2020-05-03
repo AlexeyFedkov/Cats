302c938b7a88cfd39b6aae96b21d5f1bf478ea90
nTytoAlba <ntytoalba@gmail.com>
2020-05-03 16:07:51 +0300

    Добавила тайлы

---
+++ Assets/Scripts/HideTileCollider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class HideTileCollider : MonoBehaviour
+{
+    private TilemapRenderer tilemapRender;
+
+    private void Awake()
+    {
+        tilemapRender = GetComponent<TilemapRenderer>();
+    }
+
+
+    void Start()
+    {
+        tilemapRender.enabled = false;
+    }
+
+}
